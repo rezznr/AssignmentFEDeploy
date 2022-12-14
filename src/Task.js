cc25e6889c4f944acababafbc69afeb80ef65fa0
Reza Novian Ramadhan <reza.200170172@mhs.unimal.ac.id>
2022-12-14 16:29:33 +0700

    edit : task.js

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL"; // TODO: replace this
+const url = "https://galery-app-rezanr.netlify.app/"; // TODO: replace this
 const studentName = "Reza Novian Ramadhan"; // TODO: replace this
 const studentId = "FE2557499"; // TODO: replace this
 
