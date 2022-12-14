43d950f8bb84efb9d3dad7d893abf5c1656e23e6
Reza Novian Ramadhan <reza.200170172@mhs.unimal.ac.id>
2022-12-14 18:46:33 +0700

    fix : className typo in app.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
     <>
       <div className="navbar">
         <div className="studentInfo">
-          <h2 className="stundentName">Reza Novian Ramadhan</h2>
+          <h2 className="studentName">Reza Novian Ramadhan</h2>
           <p className="studentId">FE2557499</p>
         </div>
         <Link to="/">Home</Link>
